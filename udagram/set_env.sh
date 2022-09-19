f887e5722ef822153d3c1bb414c1d9b2288b4737
hieuminh0524 <48044790+hieuminh0524@users.noreply.github.com>
2022-09-19 22:04:14 +0700

    Init

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=myPassword
+export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-west-2-750310889403
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=https://udacityapi-env.eba-zkqhwpe9.us-east-1.elasticbeanstalk.com/
